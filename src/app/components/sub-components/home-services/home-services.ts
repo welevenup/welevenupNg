7ba37a1048d832072ff87da6c5d8e2073109e02f
Deokaran Chand Thakur <deokaran.thakur10@gmail.com>
2025-07-06 17:25:03 +0530

    made the 2nd sub section of the home page

--- src/app/components/sub-components/home-services/home-services.ts
+++ src/app/components/sub-components/home-services/home-services.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import cardsjson from './cards.json'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-services',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-services.html',
   styleUrl: './home-services.scss'
 })
 export class HomeServices {
+  cards = cardsjson
+
 
 }
