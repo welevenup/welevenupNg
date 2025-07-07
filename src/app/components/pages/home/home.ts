800c63a9aaa3e4ceee5c04009cf5955236b8392a
Deokaran Chand Thakur <deokaran.thakur10@gmail.com>
2025-07-08 00:16:07 +0530

    worked on cards and started the next section not much progress but no rush

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -3,10 +3,11 @@ import { Landing } from "../../sub-components/landing/landing";
 import { WhatWeDo } from "../../sub-components/what-we-do/what-we-do";
 import { HomeServices } from "../../sub-components/home-services/home-services";
 import { Faqs } from "../../sub-components/faqs/faqs";
+import { Process } from "../../sub-components/process/process";
 
 @Component({
   selector: 'app-home',
-  imports: [Landing, WhatWeDo, HomeServices, Faqs],
+  imports: [Landing, WhatWeDo, HomeServices, Faqs, Process],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
